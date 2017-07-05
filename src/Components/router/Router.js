8d63d1f3ee5e1f35e26265c654f5f6e2de5491c3
Xinyang <xinyzhang9@gmail.com>
2017-07-05 00:28:46 -0700

    onpopstate

--- src/Components/router/Router.js
+++ src/Components/router/Router.js
@@ -24,6 +24,12 @@ export class Router extends Component{
       linkHandler: this.handleLinkClick
     }
   }
+
+  componentDidMount(){
+    window.onpopstate = ()=>{
+      this.setState({route: getCurrentPath()});
+    }
+  }
   render(){
     return <div>{this.props.children}</div>
   }
